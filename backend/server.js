b1306d354b24a037126abc3fdd3aab0e01a72395
kishanAnthwal <kishananthwal@gmail.com>
2025-03-22 14:09:23 +0530

    Update server.js
    add cors origin

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,13 @@ connectDB();
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors(
+  {
+    origin:[""],
+    methods:["POST","GET"],
+    credentials:true
+  }
+));
 
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/clicked-data', require('./routes/clickedData'));
